d7c5fc16af3fc052360541b05573e19093296c57
merantauw <zholu.vlad2010@yandex.ru>
2025-03-19 17:00:50 +0500

    finished first module

---
+++ databases/funcs.php
@@ -0,0 +1,104 @@
+<?php
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+/**
+ * Функция, которая реализует регистрацию пользователя на сайте
+ * @return bool
+ */
+function registration(): bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль обязательны';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT COUNT(*) FROM users WHERE login = ?");
+    $res->execute([$login]);
+    if ($res->fetchColumn()) {
+        $_SESSION['errors'] = 'Данное имя уже используется';
+        return false;
+    }
+
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $res = $pdo->prepare("INSERT INTO users (login, pass) VALUES (?,?)");
+
+    if ($res->execute([$login, $pass])) {
+        $_SESSION['success'] = 'Успешная регистрация';
+        return true;
+    } else {
+        $_SESSION['errors'] = 'Ошибка регистрации';
+        return false;
+    }
+}
+
+function login(): bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль обязательны';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT * FROM users WHERE login = ?");
+    $res->execute([$login]);
+
+    if (!$user = $res->fetch()) {
+        $_SESSION['errors'] = 'Пароль/логин введены неверно';
+        return false;
+    }
+
+    if (!password_verify($pass, $user['pass'])) {
+        $_SESSION['errors'] = 'Пароль/логин введены неверно';
+        return false;
+    } else {
+        $_SESSION['success'] = 'Вы успешно авторизовались';
+        $_SESSION['user']['name'] = $user['login'];
+        $_SESSION['user']['id'] = $user['id'];
+        return true;
+    }
+}
+
+function save_message(): bool
+{
+    global $pdo;
+    $message = !empty($_POST['message']) ? trim($_POST['message']) : '';
+
+    if (!isset($_SESSION['user']['name'])) {
+        $_SESSION['errors'] = 'Необходимо авторизоваться';
+        return false;
+    }
+
+    if (empty($message)) {
+        $_SESSION['errors'] = 'Введите текст сообщения';
+        return false;
+    }
+
+    $res = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?,?)");
+    if ($res->execute([$_SESSION['user']['name'], $message])) {
+        $_SESSION['success'] = 'Сообщение добавлено';
+        return true;
+    } else {
+        $_SESSION['error'] = 'Сообщение не добавлено';
+        return false;
+    }
+}
+
+function get_messages(): array
+{
+    global $pdo;
+    $res = $pdo->query("SELECT * FROM messages");
+    return $res->fetchAll();
+}
--- databases/index.php
+++ databases/index.php
@@ -2,33 +2,177 @@
 
 error_reporting(-1);
 
-function debug($data)
-{
-    echo '<pre>' . print_r($data, 1) . '</pre>';
+require_once(__DIR__ . '/db.php');
+require_once(__DIR__ . '/funcs.php');
+
+session_start();
+
+if (isset($_POST['register'])){
+    registration();
+    header('Location: index.php');
+    die;
 }
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$db = mysqli_connect('localhost', 'root', '', 'world');
+if (isset($_POST['auth'])){
+    login();
+    header('Location: index.php');
+    die;
+}
 
-$res = mysqli_query($db, "SELECT Code, Name FROM country LIMIT 10");
-//var_dump($res);
+if (isset($_POST['add'])){
+    save_message();
+    header('Location: index.php');
+    die;
+}
 
-//debug(mysqli_fetch_all($res, MYSQLI_ASSOC));
+if (isset($_GET['do']) && $_GET['do'] == 'exit'){
+    if (!empty($_SESSION['user'])){
+        unset($_SESSION['user']);
+    }
+    header('Location: index.php');
+    die;
+}
 
-/*debug(mysqli_fetch_assoc($res));
-while ($row = mysqli_fetch_assoc($res)){
-    echo 'Код страны: ' . $row['Code'] . '/ Страна: ' . $row['Name'] . '<br>';
-}*/
+$messages = get_messages();
+?>
 
-//$name = mysqli_escape_string($db, $name);
-//var_dump(mysqli_query($db, "INSERT INTO test (name) VALUES('$name')"));
-$name = "d'Artanian";
-$name2 = "Сегелёв";
-$age = 25;
-$age2 = 31;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Гостевая книга</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
 
-$query = sprintf("UPDATE test SET name = '%s', age = %d WHERE id = 3",
-    mysqli_real_escape_string($db, $name2),
-    (int)$age2);
+<div class="container">
 
-var_dump(mysqli_query($db, $query));
+    <div class="row my-3">
+        <div class="col">
+            <?php if (!empty($_SESSION['errors'])): ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?php
+                    echo $_SESSION['errors'];
+                    unset($_SESSION['errors']);
+                    ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($_SESSION['success'])): ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <?php
+                    echo $_SESSION['success'];
+                    unset($_SESSION['success']);
+                    ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <?php if (empty($_SESSION['user']['name'])): ?>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h3>Регистрация</h3>
+            </div>
+        </div>
+
+        <form action="index.php" method="post" class="row g-3">
+            <div class="col-md-6 offset-md-3">
+                <div class="form-floating mb-3">
+                    <input type="text" name="login" class="form-control" id="floatingInput" placeholder="Имя">
+                    <label for="floatingInput">Имя</label>
+                </div>
+            </div>
+
+            <div class="col-md-6 offset-md-3">
+                <div class="form-floating">
+                    <input type="password" name="pass" class="form-control" id="floatingPassword"
+                           placeholder="Password">
+                    <label for="floatingPassword">Пароль</label>
+                </div>
+            </div>
+
+            <div class="col-md-6 offset-md-3">
+                <button type="submit" name="register" class="btn btn-primary">Зарегистрироваться</button>
+            </div>
+        </form>
+
+        <div class="row mt-3">
+            <div class="col-md-6 offset-md-3">
+                <h3>Авторизация</h3>
+            </div>
+        </div>
+
+        <form action="index.php" method="post" class="row g-3">
+            <div class="col-md-6 offset-md-3">
+                <div class="form-floating mb-3">
+                    <input type="text" name="login" class="form-control" id="floatingInput" placeholder="Имя">
+                    <label for="floatingInput">Имя</label>
+                </div>
+            </div>
+
+            <div class="col-md-6 offset-md-3">
+                <div class="form-floating">
+                    <input type="password" name="pass" class="form-control" id="floatingPassword"
+                           placeholder="Password">
+                    <label for="floatingPassword">Пароль</label>
+                </div>
+            </div>
+
+            <div class="col-md-6 offset-md-3">
+                <button type="submit" name="auth" class="btn btn-primary">Войти</button>
+            </div>
+        </form>
+
+    <?php else: ?>
+
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <p>Добро пожаловать, <?= htmlspecialchars($_SESSION['user']['name']) ?>! <a href="?do=exit">Log out</a></p>
+            </div>
+        </div>
+
+        <form action="index.php" method="post" class="row g-3 mb-5">
+            <div class="col-md-6 offset-md-3">
+                <div class="form-floating">
+                <textarea class="form-control" name="message" placeholder="Leave a comment here"
+                          id="floatingTextarea" style="height: 100px;"></textarea>
+                    <label for="floatingTextarea">Сообщение</label>
+                </div>
+            </div>
+
+            <div class="col-md-6 offset-md-3">
+                <button type="submit" name="add" class="btn btn-primary">Отправить</button>
+            </div>
+        </form>
+
+    <?php endif; ?>
+
+    <?php if (!empty($messages)): ?>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <hr>
+            <?php foreach ($messages as $message): ?>
+            <div class="card my-3">
+                <div class="card-body">
+                    <h5 class="card-title">Автор: <?= htmlspecialchars($message['name']); ?></h5>
+                    <p class="card-text"><?= nl2br(htmlspecialchars($message['message'])); ?></p>
+                    <p>Дата: <?= $message['created_at']; ?></p>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <?php endif; ?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+</html>
